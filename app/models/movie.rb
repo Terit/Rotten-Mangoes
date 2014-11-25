fd57524b2a75bcb96070565d4d637af1eb4b6803
Andy Theriault <artheriault@gmail.com>
2014-11-25 01:08:05 +0000

    step 5 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ActiveRecord::Base
+
+  validates :title, :director, :description, :release_date, :poster_image_url, presence: true
+  validates :runtime_in_minutes, numericality: { only_integer: true }
+  validate :release_date_is_in_the_future
+
+  protected
+
+  def release_date_is_in_the_future
+    if release_date.present?
+      errors.add(:release_date, "should probably be in the future") if release_date < Date.today
+    end
+  end
 end
