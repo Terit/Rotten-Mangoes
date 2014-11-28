a023c2f164a28f9b3be8dbc1f374a3680f579467
Andy Theriault <artheriault@gmail.com>
2014-11-28 04:12:02 +0000

    working search bar

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,22 +8,28 @@ class Movie < ActiveRecord::Base
 
   scope :search_results, -> (params) do 
     if params[:title].present?
-      where("title LIKE ?", "%#{params[:title]}%") 
+      find_by_title(params[:title]).movie_duration(params[:duration])
     elsif params[:director].present?
-      where("director LIKE ?", "%#{params[:director]}%")
+      find_by_director(params[:director]).movie_duration(params[:duration])
+    else
+      movie_duration(params[:duration])
     end
-    if params[:duration].present?
-      case params[:duration]
+  end
+
+  scope :find_by_title, -> (title) { where("title LIKE ?", title) }
+  scope :find_by_director, -> (name) { where("director LIKE ?", "%#{name}%") }
+  scope :movie_duration, -> (duration) do
+      case duration
       when "1"
         where("runtime_in_minutes < ?", 90)
       when "2"
         where("runtime_in_minutes BETWEEN ? AND ?", 90, 120)
       when "3"
         where("runtime_in_minutes > ?", 120)
+      else
+        where("runtime_in_minutes > 0")
       end
-    end
   end
-
 
   def review_average
     if reviews.size > 0
