ffc5b8f16974e85c7d6928dd07887a0dc29adc8a
Andy Theriault <artheriault@gmail.com>
2014-11-28 02:02:44 +0000

    working search features, still needs styling

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,10 +2,28 @@ class Movie < ActiveRecord::Base
   has_many :reviews
   mount_uploader :poster, PosterUploader
   
-  
   validates :title, :director, :description, :release_date, presence: true
   validates :runtime_in_minutes, numericality: { only_integer: true }
   validate :release_date_is_in_the_future
+
+  scope :search_results, -> (params) do 
+    if params[:title].present?
+      where("title LIKE ?", "%#{params[:title]}%") 
+    elsif params[:director].present?
+      where("director LIKE ?", "%#{params[:director]}%")
+    end
+    if params[:duration].present?
+      case params[:duration]
+      when "1"
+        where("runtime_in_minutes < ?", 90)
+      when "2"
+        where("runtime_in_minutes BETWEEN ? AND ?", 90, 120)
+      when "3"
+        where("runtime_in_minutes > ?", 120)
+      end
+    end
+  end
+
 
   def review_average
     if reviews.size > 0
